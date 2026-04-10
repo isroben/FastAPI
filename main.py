ff53f1280cd9a1660f9ae6bd9200accbb86a8991
isroben <robenthapaaa@gmail.com>
2026-04-10 09:16:16 +0545

    Query params

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 from dataLoader import loadData
 
 
@@ -28,3 +28,24 @@ def viewPatient(patientId: str = Path(..., description="ID of the patient in the
     if patientId in data:
         return data[patientId]
     raise HTTPException(404, "Patient Not Found!")
+
+
+@app.get("/sort")
+def sortPatients(sortby: str = Query(..., description="Sort on the basis of height, weight, bmi."), order: str = Query('asc', description="Sort by asc or desc order.")):
+    validFields = ['height', 'weight', 'bmi']
+    validOrders = ['asc', 'desc']
+
+    if sortby not in validFields:
+        raise HTTPException(400, f"Invalid field selected from {validFields}")
+    
+    if order not in validOrders:
+        raise HTTPException(400, "Invalid order to sort.")
+    
+
+    data = loadData()
+
+    sortOrder = True if order == 'desc' else False
+
+    sortedData = sorted(data.values(), key=lambda x: x.get(sortby, 0), reverse=sortOrder)
+
+    return sortedData
